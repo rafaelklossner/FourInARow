4861f470f419c9115a4b148430211b4a766ae54d
Unknown <klossner1994@bluewin.ch>
2018-05-15 17:27:19 +0200

    change grid

--- src/GameGrid.java
+++ src/GameGrid.java
@@ -1,10 +1,23 @@
 
 public class GameGrid {
-	private int xSize;
+	private int xSize = 7;
+	private int ySize = 6;
 	private int grid[][];
 	
 	GameGrid(){
-		grid = new int[7][6];
-		for(int i=0; i>)
+		grid = new int[xSize][ySize];
+		for(int x=0; x < xSize; x++) { // init grid
+			for(int y=0; y < ySize; y++) {
+				grid[x][y] = 0;
+			}
+		}
+	}
+	
+	void placeStone(){
+		
+	}
+	
+	int[][] getGrid() {
+		return grid;
 	}
 }
