961e1f9fc95538c2d6a812e4f63ca8045cee6e5a
rafaelklossner <32688686+rafaelklossner@users.noreply.github.com>
2018-05-15 16:14:00 +0200

    Upload base project

---
+++ FourInARow/src/FourInARow.java
@@ -0,0 +1,9 @@
+
+public class FourInARow {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ FourInARow/src/Player.java
@@ -0,0 +1,14 @@
+
+public class Player {
+	int index;
+	String name;
+	String color;
+	int points;
+	
+	Player(int index, String name, String color, int points){
+		this.index = index;
+		this.name = name;
+		this.color = color;
+		this.points = points;
+	}
+}
